df9df9ad400d68c3bb2d0ee66813368c04e6b22c
julian-blohm <julian@startup-werk.de>
2024-08-15 12:01:23 +0200

    add finding in lists

---
+++ 16_finding-in-lists.py
@@ -0,0 +1,13 @@
+# finding things in lists
+# element in list; returns boolean
+letters = ["A", "B", "C", "D", "E"]
+print("B" in letters)
+print("")
+print("Z" in letters)
+print("")
+
+# element not in list
+print("B" not in letters)
+print("")
+print("Z" not in letters)
+print("")
